98bb130298dd226cba560c7bbcf200aa862f0d3c
Gabe Nuñez <gabe@gabenunez.com>
2018-08-27 15:46:52 -0700

    Add grand view typography theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,5 +1,6 @@
 import Typography from "typography";
+import grandViewTheme from 'typography-theme-grand-view'
 
-const typography = new Typography({ baseFontSize: "18px" });
+const typography = new Typography(grandViewTheme);
 
 export default typography;
