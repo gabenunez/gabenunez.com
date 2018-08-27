3a0bcbad2029a3e11917dc54ec8a414f8a5fc28a
Gabe Nuñez <gabe@gabenunez.com>
2018-08-27 15:34:58 -0700

    Add typography plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,8 @@ module.exports = {
   siteMetadata: {
     title: 'Gabe Nunez | Web Developer',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-typography`
+  ],
 }
