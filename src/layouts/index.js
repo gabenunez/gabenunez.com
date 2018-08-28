c4b343cd87b178aff7e323b2dbff5f8deeba7f0d
Gabe Nuñez <gabe@gabenunez.com>
2018-08-28 00:53:57 -0700

    Add Chiques favicon! 🐱

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import favicon from '../images/favicon.png';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 
@@ -12,6 +13,9 @@ const Layout = ({ children, data }) => (
         { name: 'description', content: 'Gabe Nunez is web developer located in the San Francisco Bay Area... and really enjoys javascript :).' },
         { name: 'keywords', content: 'web development, web, website creating, website creator, web programmer' },
       ]}
+      link={[
+        { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` }
+    ]}
     />
     {children()}
   </div>
