9f9391f37d14c82d0afdb4cd2679b9faab7e760f
Gabe Nuñez <gabe@gabenunez.com>
2018-08-28 18:38:04 -0700

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,13 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    `gatsby-plugin-typography`
-  ],
+    `gatsby-plugin-typography`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-124809202-1',
+        head: true
+      },
+    },
+    ]
 }
