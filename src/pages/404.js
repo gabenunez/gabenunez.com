8420fdd35700cf9b9da37df7307465c8fb9c6173
Gabe Nuñez <gabe@gabenunez.com>
2018-08-28 18:11:09 -0700

    Styling for the 404 page!

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import { Jumbotron, Container, Row, Col } from 'reactstrap'
+import Link from 'gatsby-link'
+import {Helmet} from "react-helmet";
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+  <Container className='align-items-center h-100'>
+
+    <Helmet>
+      <title>404 | Gabe Nunez</title>
+      <meta name="description" content="404 Page... Sorry :(... maybe this page will exist one day?" />
+      <meta name="robots" content="noindex" />
+    </Helmet>
+
+    <Row>
+      <Col md='12' className='text-center'>
+        <div className='error-box'>
+          <h1>404 NOT FOUND</h1>
+          <p>Maybe... just maybe... you should <Link
+          to="/">go to the homepage?</Link></p>
+        </div>
+      </Col>
+    </Row>
+  </Container>
 )
 
 export default NotFoundPage
